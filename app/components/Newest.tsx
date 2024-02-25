136acee71cdcadf4eceda949e44e7e87f91f0bcf
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-02-25 14:30:26 +0200

    Styles in Newest file and new file for product type and small update in page.

--- app/components/Newest.tsx
+++ app/components/Newest.tsx
@@ -1,4 +1,7 @@
+import Link from "next/link";
+import { simplifiedProduct } from "../interface";
 import { client } from "../lib/sanity";
+import { ArrowRight } from "lucide-react";
 
 async function getData() {
   const query = `*[_type == 'product'][0...4] | order(_createdAt desc) {
@@ -14,5 +17,30 @@ async function getData() {
 }
 
 export default async function Newest() {
-  const data = await getData();
+  const data: simplifiedProduct[] = await getData();
+
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <div className="flex justify-between items-center">
+          <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+            Our Newest Products
+          </h2>
+          <Link className="text-primary flex items-center gap-x-1" href="/all">
+            See all
+            <span>
+              <ArrowRight />
+            </span>
+          </Link>
+        </div>
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {data.map((product) => (
+            <div key={product._id} className="group relative">
+              <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80"></div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
